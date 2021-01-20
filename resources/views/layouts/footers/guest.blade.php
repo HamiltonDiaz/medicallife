c79a67da941943178cee091ec277dd26f5c18311
HamiltonDiaz <diaz3220@hotmail.com>
2021-01-20 00:10:56 -0500

    inicio
    inicio proyecto

---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="container">
+        <nav class="float-center">
+        <ul>
+            <li>
+            <a href="https://www.creative-tim.com">
+                {{ __('Contactenos') }}
+            </a>
+            </li>
+           
+        </ul>
+        </nav>
+
+    </div>
+</footer>
