25b984e22d8d608b78fc66c7923df89de101f781
HamiltonDiaz <diaz3220@hotmail.com>
2021-02-10 21:53:42 -0500

    implementación del formulario de contactenos

---
+++ app/Http/Controllers/LineController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Line;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class LineController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //Almacenar la imagen
+        $path = $request->file('avatar')->store('avatars');
+
+        return $path;
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Line  $line
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Line $line)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Line  $line
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Line $line)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Line  $line
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Line $line)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Line  $line
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Line $line)
+    {
+        //
+    }
+}
